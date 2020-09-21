a26472e3618102ab1f54da01191aaf654cb0d78f
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-09-21 15:27:35 -0500

    Updates

---
+++ precache-manifest.ec8667a5571278b0aecd50b818adc0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aa5417417dec81b8e3c",
+    "url": "/react-use-stateful-form/static/css/main.eb9a8b12.chunk.css"
+  },
+  {
+    "revision": "7aa5417417dec81b8e3c",
+    "url": "/react-use-stateful-form/static/js/main.4175cfab.chunk.js"
+  },
+  {
+    "revision": "318616b9006b2bfbc66b",
+    "url": "/react-use-stateful-form/static/js/runtime~main.d358c936.js"
+  },
+  {
+    "revision": "04dfa6f49da71a303678",
+    "url": "/react-use-stateful-form/static/js/2.6c2e770a.chunk.js"
+  },
+  {
+    "revision": "ba7da3b18ad1d955e3707faa8568470a",
+    "url": "/react-use-stateful-form/index.html"
+  }
+];
